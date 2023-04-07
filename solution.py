aacf29f06d38edf95a149537c2d1a0dc352d0d1b
Kirill-N-M <130153965+Kirill-N-M@users.noreply.github.com>
2023-04-07 19:46:33 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,5 +5,5 @@ import numpy as np
 chat_id = 586939927 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    a = (x.mean()-1)/128
+    a = (x.mean())/128
     return a # Ваш ответ
